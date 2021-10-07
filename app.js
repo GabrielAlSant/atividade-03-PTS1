fcce861ac32d4ae42662534fdc866eef37811ca0
Gabriel Alves dos Santos <86124609+GabrielAlSant@users.noreply.github.com>
2021-10-07 14:01:10 +0000

    rotas prontas

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var noticiaRouter = require('./routes/noticia');
+var rerunRouter = require('./routes/rerun');
+
 
 var app = express();
 
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/noticia', noticiaRouter);
+app.use('/rerun', rerunRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
